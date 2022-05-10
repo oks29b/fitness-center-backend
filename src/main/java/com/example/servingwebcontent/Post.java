699428fbbb7086a8805e9f53ad1e6ebd3f9adb99
Aksana Barysenka <oksana_borisenko.95@example.com>
2022-05-10 22:57:01 +0300

    connected mariadb

--- src/main/java/com/example/servingwebcontent/Post.java
+++ src/main/java/com/example/servingwebcontent/Post.java
@@ -14,6 +14,15 @@ public class Post {
     private String title, anons, full_text;
     private int views;
 
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+
+    public Post() {
+    }
+
     public Long getId() {
         return id;
     }
