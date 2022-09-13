c5bc16bf8d1bd7f554db99d3d0fc7963fca31f03
Oksana <oksana@mail.ru>
2022-09-13 16:36:38 +0300

    added lombok

--- src/main/java/com/example/servingwebcontent/model/entity/Users.java
+++ src/main/java/com/example/servingwebcontent/model/entity/Users.java
@@ -1,11 +1,16 @@
 package com.example.servingwebcontent.model.entity;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name = "usr")
+@Getter
+@Setter
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,44 +24,4 @@ public class Users {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
